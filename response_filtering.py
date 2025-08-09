750a56bfaec01e8d842d22d013dcdc6f84b395d3
joshua <chengjoshua@email.com>
2025-08-09 17:14:02 -0500

    edit responses

--- response_filtering.py
+++ response_filtering.py
@@ -11,18 +11,41 @@ for model_response_fp in os.listdir(model_response_directory):
         for question in model_response:
             if question["is_correct"] == False and len(question["response"]) != 1:
                 response = question["response"]
+                original_response = str(question["response"])
                 edit_response = response.replace('-', '').strip()
+                alternate_correct_answer = ""
 
-                save = {
-                    "model" : model_response_fp.replace(".json", ""),
-                    "response" : edit_response,
-                    "options" : question["options"],
-                    "correct_answer" : question["correct_answer"]
-                }
-                incorrect_answers.append(save)
+                
 
                 question["response"] = edit_response
-                question["is_correct"] = True if edit_response.upper() == question["correct_answer"].upper() else False
+                # question["is_correct"] = True if edit_response.upper() == question["correct_answer"].upper() else False
+                if edit_response.upper() == question["correct_answer"].upper():
+                    question["is_correct"] = True
+                else:
+                    if question["correct_answer"].upper().strip() == "A":
+                        alternate_correct_answer = question["options"][0][4:].replace('-', '').strip()
+                    elif question["correct_answer"].upper().strip() == "B":
+                        alternate_correct_answer = question["options"][1][4:].replace('-', '').strip()
+                    elif question["correct_answer"].upper().strip() == "C":
+                        alternate_correct_answer = question["options"][2][4:].replace('-', '').strip()
+                    elif question["correct_answer"].upper().strip() == "D":
+                        alternate_correct_answer = question["options"][3][4:].replace('-', '').strip()
+
+                    if alternate_correct_answer.upper() == edit_response.upper():
+                        question["is_correct"] = True
+                    else:
+                        question["is_correct"] = False
+                
+                save = {
+                    "model" : model_response_fp.replace(".json", ""),
+                    "original_response" : original_response,
+                    "edit_response" : edit_response,
+                    "options" : question["options"],
+                    "correct_answer" : question["correct_answer"],
+                    "alternate_correct_answer" : alternate_correct_answer
+                }
+                incorrect_answers.append(save)
+                        
             edit_model_response.append(question)
 
     with open(f"{model_response_directory}/{model_response_fp}", "w") as f:
