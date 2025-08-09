41bffa96c4af502fc647412d0eea698354544406
joshua <chengjoshua@email.com>
2025-08-09 18:02:18 -0500

    analyzing the responses

---
+++ response_analysis.py
@@ -0,0 +1,16 @@
+import os
+import json
+
+model_response_directory = "test_quiz_results"
+
+for model_response_fp in os.listdir(model_response_directory):
+    count = 0
+    num_questions = 0
+    with open(os.path.join(model_response_directory, model_response_fp), "r") as f:
+        model_responses = json.load(f)
+        for response in model_responses:
+            if response["is_correct"] == True:
+                count += 1
+            num_questions += 1
+    print(f"Model: {model_response_fp}, Correct: {count}, Total: {num_questions}")
+                
